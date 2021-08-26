44632e57a20bc20ad2a7ebcd511a7ae9071cf5bd
Anne Olsen <annejko@hotmail.com>
2021-08-26 11:45:55 +0200

    done

---
+++ 1._Intro/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+       Systemos.out.println("hello");
+       Systemos.out.println(6);
+       Systemos.out.println(6.8);
+
+    }
+}
---
+++ 1._Intro/Systemos.java
@@ -0,0 +1,31 @@
+public class Systemos {
+
+
+    public static class out{
+
+        public static void println(String text) {
+            System.out.println(text);
+        }
+
+        public static void println(int number) {
+            System.out.println(number);
+        }
+        public static void println(double number) {
+            System.out.println(number);
+        }
+
+        public static void println(char text) {
+            System.out.println(text);
+        }
+
+        public static void println(float number) {
+            System.out.println(number);
+        }
+
+        public static void println(long number) {
+            System.out.println(number);
+        }
+
+
+        }
+}
