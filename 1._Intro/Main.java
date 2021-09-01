25fff43f1a4f003f78c6848a611df9a950a369e2
Anne Olsen <annejko@hotmail.com>
2021-09-01 09:28:52 +0200

    2.

--- 1._Intro/Main.java
+++ 1._Intro/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
 
 
@@ -7,5 +9,40 @@ public class Main {
        Systemos.out.println(6);
        Systemos.out.println(6.8);
 
+       //implement word , give it content that you
+       //think makes sense and allow it to be printed out with Systemos
+
+       Systemos.out.println(new Word("Hej"));
+
+       //make an array of 5 words and implement a method in Systemos that prints them out
+
+        Word[] words = new Word[]{
+                new Word("Hej"),
+                new Word("med"),
+                new Word("dig"),
+                new Word("!"),
+                new Word("!!"),
+        };
+
+        Systemos.out.println(words);
+
+
+        // create an arrayList of several Words and
+        // implement a way to print them out with Systemos
+
+        ArrayList<Word> wordlist = new ArrayList<>(){{
+            add(new Word("bord"));
+            add(new Word("TV"));
+            add(new Word("seng"));
+            add(new Word("stol"));
+            add(new Word("hej"));
+        }};
+        Systemos.out.println(wordlist);
+
+
+
     }
+
+
+
 }
--- 1._Intro/Systemos.java
+++ 1._Intro/Systemos.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Systemos {
 
 
     public static class out{
 
-        public static void println(String text) {
-            System.out.println(text);
-        }
+       // public static void println(String text) {
+         //   System.out.println(text);
+        //}
 
         public static void println(int number) {
             System.out.println(number);
@@ -27,5 +31,25 @@ public class Systemos {
         }
 
 
+        public static void println(Object content) {
+            System.out.println(content);
+
         }
+
+        public static void println(Object[]content) {
+            for (Object object : content) {
+                println(object);
+            }
+        }
+
+        // nedenstående hedder lambda
+        public static void println(List content) {
+                content.forEach(object -> println(object));
+            }
+
+
+
+
+
+    }
 }
